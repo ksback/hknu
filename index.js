bf001cbf97b7f3e356a7e3820ccf555955d83425
ksabck <136818625+ksback@users.noreply.github.com>
2024-10-16 22:52:47 +0900

    Update index.js
    토큰추가

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const token = 'YOUR_PERSONAL_ACCESS_TOKEN'; // 발급받은 토큰
+
+fetch('https://api.github.com/user/repos', {
+    method: 'GET',
+    headers: {
+        'Authorization': `token ${token}`,
+        'Accept': 'application/vnd.github.v3+json'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data); // 리포지토리 정보 출력
+})
+.catch(error => {
+    console.error('Error:', error);
+});
+
 const imageCache = {}; // 이미지 캐시 객체
 const imagePaths = new Set(); // 중복 방지를 위한 Set
 
