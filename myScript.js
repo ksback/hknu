e6ee0371f1de214833db4d1a5658938a163e3432
ksabck <136818625+ksback@users.noreply.github.com>
2024-10-17 01:01:19 +0900

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,3 +1,36 @@
+const token = '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'; // 발급받은 토큰
+
+fetch('https://api.github.com/user/repos', {
+    method: 'GET',
+    headers: {
+        'Authorization': `token ${token}`,
+        'Accept': 'application/vnd.github.v3+json'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data); // 리포지토리 정보 출력
+})
+.catch(error => {
+    console.error('Error:', error);
+});
+
+
+fetch('https://api.github.com/rate_limit', {
+    method: 'GET',
+    headers: {
+        'Authorization': `token ${token}`,
+        'Accept': 'application/vnd.github.v3+json'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data); // Rate limit 정보 출력
+})
+.catch(error => {
+    console.error('Error:', error);
+});
+
 const csvUrl = 'https://raw.githubusercontent.com/ksback/hknu/refs/heads/main/hknu1016.csv';
 
 const colorMap = {
